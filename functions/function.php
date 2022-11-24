ff5895e7498631e33b34c3f80625ce8f7c34e092
Vicky <kymavic18@hotmail.com>
2022-11-24 11:55:55 +1000

    add safe point before make static the whole page

--- functions/function.php
+++ functions/function.php
@@ -1,5 +1,32 @@
 <?php
 
+//Characters
+ 
+$GLOBALS["characters"] = Array();
+
+function listCharacters($dbConnect){
+    return $GLOBALS["characters"];
+}
+
+function saveCharacter($dbConnect, $id, $img, $name, $description){
+    $character = Array(
+        "id" => $id,
+        "img" => $img,
+        "name" => $name,
+        "description" => $description
+    );
+    array_push($GLOBALS["characters"],$character);
+    return $character;
+}
+
+saveCharacter(null,"1","https://upload.wikimedia.org/wikipedia/en/9/9b/Luke_Skywalker.png","Luke Skywalker"," Luke is the twin brother of Rebellion leader Princess Leia and eventual brother-in-law of the smuggler Han Solo. Luke trains to be a Jedi under Jedi Masters Obi-Wan Kenobi and Yoda and rebuilds the Jedi Order.");
+saveCharacter(null,"2","https://upload.wikimedia.org/wikipedia/en/1/1b/Princess_Leia%27s_characteristic_hairstyle.jpg","Leia Skywalker","Leia is princess of the planet Alderaan, a member of the Imperial Senate and an agent of the Rebel Alliance. She thwarts the sinister Sith Lord Darth Vader and helps bring about the destruction of the Empire's cataclysmic superweapon, the Death Star.");
+saveCharacter(null,"3","https://upload.wikimedia.org/wikipedia/en/9/9b/Yoda_Empire_Strikes_Back.png","Yoda","He is a small, green humanoid alien who is powerful with the Force and is a leading member of the Jedi Order until its near annihilation.");
+saveCharacter(null,"4","https://upload.wikimedia.org/wikipedia/en/0/0b/Darth_Vader_in_The_Empire_Strikes_Back.jpg","Anakin Skywalker (Darth Vader)","The character is the central antagonist of the original trilogy and, as Anakin Skywalker, is one of the main protagonists throughout the prequel trilogy.");
+saveCharacter(null,"5","https://upload.wikimedia.org/wikipedia/en/b/be/Han_Solo_depicted_in_promotional_image_for_Star_Wars_%281977%29.jpg","Han Solo","Han Solo and his first mate Chewbacca are smugglers who are hired by Ben Kenobi and Luke Skywalker to transport them to Alderaan so they can deliver the stolen plans for the Death Star.");
+saveCharacter(null,"6","https://upload.wikimedia.org/wikipedia/en/1/12/Chewbaca_%28Peter_Mayhew%29.png","Chewbacca","He is a Wookiee, a tall, hirsute, bipedal, intelligent species originating from the fictional planet of Kashyyyk. Chewbacca is the loyal friend and first mate of Han Solo, and serves as co-pilot on Solo's spaceship, the Millennium Falcon; together they help the Rebel Alliance defeat the Galactic Empire and restore freedom to the galaxy.");
+
+/*
 function dbLink()
 {
     $db_user = '<Datbase User>';
@@ -50,3 +77,4 @@ function '<function name>'($dbConnect,$id){
     $stmt->bindParam(':id',$id);
     $stmt->execute();
 }
+*/
--- pages/characters.php
+++ pages/characters.php
@@ -4,45 +4,9 @@ session_start();
 if($_SESSION['user'] == null){
     header("Location: login.php");
 }*/
-
-$character1 = Array(
-  "id" => "1",
-  "img" => "https://upload.wikimedia.org/wikipedia/en/9/9b/Luke_Skywalker.png",
-  "name" => "Luke Skywalker",
-  "description" => " Luke is the twin brother of Rebellion leader Princess Leia and eventual brother-in-law of the smuggler Han Solo. Luke trains to be a Jedi under Jedi Masters Obi-Wan Kenobi and Yoda and rebuilds the Jedi Order."
-);
-$character2 = Array(
-  "id" => "2",
-  "img" => "https://upload.wikimedia.org/wikipedia/en/1/1b/Princess_Leia%27s_characteristic_hairstyle.jpg",
-  "name" => "Leia Skywalker",
-  "description" => "Leia is princess of the planet Alderaan, a member of the Imperial Senate and an agent of the Rebel Alliance. She thwarts the sinister Sith Lord Darth Vader and helps bring about the destruction of the Empire's cataclysmic superweapon, the Death Star."
-);
-$character3 = Array(
-  "id" => "3",
-  "img" => "https://upload.wikimedia.org/wikipedia/en/9/9b/Yoda_Empire_Strikes_Back.png",
-  "name" => "Yoda",
-  "description" => "He is a small, green humanoid alien who is powerful with the Force and is a leading member of the Jedi Order until its near annihilation."
-);
-$character4 = Array(
-  "id" => "4",
-  "img" => "https://upload.wikimedia.org/wikipedia/en/0/0b/Darth_Vader_in_The_Empire_Strikes_Back.jpg",
-  "name" => "Anakin Skywalker (Darth Vader)",
-  "description" => "The character is the central antagonist of the original trilogy and, as Anakin Skywalker, is one of the main protagonists throughout the prequel trilogy."
-);
-$character5 = Array(
-  "id" => "5",
-  "img" => "https://upload.wikimedia.org/wikipedia/en/b/be/Han_Solo_depicted_in_promotional_image_for_Star_Wars_%281977%29.jpg",
-  "name" => "Han Solo",
-  "description" => "Han Solo and his first mate Chewbacca are smugglers who are hired by Ben Kenobi and Luke Skywalker to transport them to Alderaan so they can deliver the stolen plans for the Death Star."
-);
-$character6 = Array(
-  "id" => "6",
-  "img" => "https://upload.wikimedia.org/wikipedia/en/1/12/Chewbaca_%28Peter_Mayhew%29.png",
-  "name" => "Chewbacca",
-  "description" => "He is a Wookiee, a tall, hirsute, bipedal, intelligent species originating from the fictional planet of Kashyyyk. Chewbacca is the loyal friend and first mate of Han Solo, and serves as co-pilot on Solo's spaceship, the Millennium Falcon; together they help the Rebel Alliance defeat the Galactic Empire and restore freedom to the galaxy."
-);
+include_once('../functions/function.php');
 global $characters;
-$characters = Array($character1,$character2,$character3,$character4,$character5,$character6);
+$characters = listCharacters(null);
 
 ?>
 
